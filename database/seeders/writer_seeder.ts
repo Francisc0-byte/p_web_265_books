5b499dddf88d0176b03be65e3cb08200bcb0bb66
RED-DFRL <ps01nnt@eduvaud.ch>
2025-09-26 15:39:21 +0200

    Feat(Seeder): Ajout des seeders

--- database/seeders/writer_seeder.ts
+++ database/seeders/writer_seeder.ts
@@ -1,7 +1,8 @@
+import { WriterFactory } from '#database/factories/writer_factory'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+    await WriterFactory.createMany(10)
   }
 }
