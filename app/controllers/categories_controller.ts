89e2b649d94babf67c2616bd3ae570dc046f6718
Francisc0-byte <pc96ihd@eduvaud.ch>
2025-09-26 13:42:58 +0200

    Feat
    Affichage de l'index category

--- app/controllers/categories_controller.ts
+++ app/controllers/categories_controller.ts
@@ -1,10 +1,13 @@
 import type { HttpContext } from '@adonisjs/core/http'
-
+import Category from '#models/category'
 export default class CategoriesController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({response}: HttpContext) {
+    const category = await Category.query().orderBy('id')
+    return response.ok(category)
+  }
   /**
    * Handle form submission for the create action
    */
