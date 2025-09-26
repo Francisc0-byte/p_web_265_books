aa970a0b709c205c299c3d1d8795c7e98cedf99a
RED-DFRL <ps01nnt@eduvaud.ch>
2025-09-26 13:59:32 +0200

    Feat(Models): Création du model de Writer

--- app/models/writer.ts
+++ app/models/writer.ts
@@ -1,13 +1,12 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class Writer extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  @column()
+  declare lastname: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  @column()
+  declare firstname: number
 }
